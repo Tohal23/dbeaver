b3a01011f30c8c047642a235c3a0c8320eb5b31d
Anton <krivonosenko98@gmail.com>
2020-03-30 22:08:55 +0300

    Added translation of three strings on Russian on the last page of Data Transfer wizard

--- plugins/org.jkiss.dbeaver.tasks.ui/src/org/jkiss/dbeaver/tasks/ui/internal/TaskUIMessages.java
+++ plugins/org.jkiss.dbeaver.tasks.ui/src/org/jkiss/dbeaver/tasks/ui/internal/TaskUIMessages.java
@@ -23,6 +23,9 @@ public class TaskUIMessages extends NLS {
 	static final String BUNDLE_NAME = "org.jkiss.dbeaver.tasks.ui.internal.TaskUIMessages"; //$NON-NLS-1$
 
 	public static String task_config_wizard_group_task_label;
+	public static String task_config_wizard_job_task_save;
+	public static String task_config_wizard_output_tasks_view;
+	public static String task_config_wizard_output_view_show;
 
 	static {
 		// initialize resource bundle
